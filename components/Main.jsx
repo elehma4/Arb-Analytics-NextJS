7ecd32208c6c63e0a937f146a3a337255fda4481
Hunter Atkins <lsatkins111@gmail.com>
2023-06-29 16:53:36 -0500

    layout for nav and main components functioning as intended with changes in screen size

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const Main = () => {
+const Main = ( {isSmallScreen} ) => {
+
+
   return (
 
     <div id='home' className='w-full h-screen text-center'>
 
-        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+        <div className={` relative ${isSmallScreen ? 'top-14' : 'left-56'} bg-gray-800 max-w-[1240px] h-full mx-auto p-2 flex justify-center items-center `}>
           
-          <div>Main</div>
+          <div className='bg-gray-900 h-24 flex flex-col'></div>
 
         </div>
 
