c99a9bf0811399466df61db589f8d54f5e63ddb3
Hunter Atkins <lsatkins111@gmail.com>
2023-06-30 12:24:45 -0500

    fixed issue on really big screens

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import './main.css'
 
 const Main = ( {isSmallScreen} ) => {
 
 
   return (
 
-    <div id='home' className='w-full h-screen text-center'>
+    <div id='home' className='h-screen text-center'>
 
-        <div className={` relative ${isSmallScreen ? 'top-14' : 'left-56'} bg-gray-800 max-w-[1240px] h-full mx-auto p-2 flex justify-center items-center `}>
+          <div className={` relative ${isSmallScreen ? 'top-14 mainSmall' : 'left-56 mainBig'} bg-gray-800 h-full mx-auto p-2 flex justify-center items-center`}>
           
           <div className='bg-gray-900 h-24 flex flex-col'></div>
 
---
+++ client/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+   <>
+
+    <div>
+
+        <form>
+
+        </form>
+
+
+    </div>
+   
+    
+   
+   </>
+  )
+}
+
+export default Search
