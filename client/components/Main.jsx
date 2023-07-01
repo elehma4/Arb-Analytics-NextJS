a548a8439592e9ecf76f9cbe9556d401311ca46e
ETH1Elohim <ezmax.defi@gmail.com>
2023-07-01 09:36:24 -0500

    more mobile friendly

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -12,7 +12,7 @@ const Main = ( {isSmallScreen} ) => {
 
         <div className={` relative ${isSmallScreen ? 'top-14 mainSmall' : 'left-56 mainBig'} bg-gray-800 h-full mx-auto p-4 flex flex-col justify-start items-center`}>
           
-          <form className='bg-gray-900 relative rounded-3xl w-full h-24 flex items-start justify-center ml-2 mr-4 flex-col'>
+          <form className='bg-gray-900 relative rounded-3xl w-full h-24 flex items-start justify-center mx-2 sm:mr-4 flex-col'>
             <div className='w-full h-1/2 rounded-t-3xl outline-none bg-black text-white p-2 px-4 flex items-center'>
               <BsSearch className='mr-2 text-gray-400' />
               <input className='w-full bg-transparent outline-none text-white placeholder-gray-400' type="search" placeholder="Search" />
@@ -28,31 +28,42 @@ const Main = ( {isSmallScreen} ) => {
           </form>
 
 
-          <div className='w-full h-3/5 bg-gray-900 rounded-3xl mt-6 flex p-6 text-gray-400'>
-              <div className='font-bold flex flex-col'>
-                <p className='text-xl m-1'>Total Value Locked </p>
-                <p className='text-2xl mb-4 text-white m-1'>$2.17b</p>
-                <p className='text-lg m-1 mb-2'>24hr Change</p>
-                <p className='text-lg text-white mb-2 ml-1'>0.46%</p>
-                <p className='text-lg m-1 mb-2'>24hr Fees</p>
-                <p className='text-lg text-white mb-2 ml-1'>$205,417</p>
-                <p className='text-lg m-1'>$ARB Price</p>
-                <p className='text-lg text-white mb-2 ml-1'>$1.17</p>
-                <p className='text-lg m-1'>$ARB Volume</p>
-                <p className='text-lg text-white mb-2 ml-1'>$1.17</p>
+          <div className='w-full h-3/5 bg-gray-900 rounded-3xl mt-6 p-6 text-gray-400 sm:grid grid-rows-3 grid-flow-col gap-4'>
+              <div className='font-bold flex sm:flex-col row-span-3'>
+                <div>
+                <p className='sm:text-xl m-1 text-sm'>Total Value Locked </p>
+                <p className='sm:text-2xl sm:mb-4 text-white m-1'>$2.17b</p>
+                </div>
+                <div>
+                <p className='sm:text-lg m-1 mb-2 text-sm'>24hr Change</p>
+                <p className='sm:text-lg text-white sm:mb-2 ml-1'>0.46%</p>
+                </div>
+                <div>
+                <p className='sm:text-lg m-1 mb-2 text-sm'>24hr Fees</p>
+                <p className='sm:text-lg text-white sm:mb-2 ml-1'>$205,417</p>
+                </div>
+                <div>
+                <p className='sm:text-lg m-1 text-sm'>$ARB Price</p>
+                <p className='sm:text-lg text-white sm:mb-2 ml-1'>$1.17</p>
+                </div>
+                <div>
+                <p className='sm:text-lg m-1 text-sm'>$ARB Volume</p>
+                <p className='sm:text-lg text-white sm:mb-2 ml-1'>$1.17</p>
+                </div>
+                
               </div>
               
-              <div className='w-full'>
-              <div className='w-full h-auto m-auto flex items-start justify-start pl-6 p-4 font-normal'>
+              
+              <div className='w-full h-auto m-auto flex items-start justify-start sm:pl-6 sm:p-4 font-normal col-span-2'>
                 <button className='bg-[#3267D6] hover:scale-105 ease-in duration-300 rounded-2xl py-2 px-3 m-2 text-gray-200'>TVL</button>
                 <button className='bg-[#3267D6] hover:scale-105 ease-in duration-300 rounded-2xl py-2 px-3 m-2 text-gray-200'>Fees</button>
                 <button className='bg-[#3267D6] hover:scale-105 ease-in duration-300 rounded-2xl py-2 px-3 m-2 text-gray-200'>$ARB Price</button>
                 <button className='bg-[#3267D6] hover:scale-105 ease-in duration-300 rounded-2xl py-2 px-3 m-2 text-gray-200'>$ARB Volume</button>
               </div>
-              <div className='border-2 w-full h-[80%] ml-4'>
+              <div className='border-2 w-full row-span-2 col-span-2'>
                   Chart goes here
               </div>
-              </div>
+              
               
 
           </div>            
