b5401f489da0858f7aa351cd550db8ea689d5ad4
ETH1Elohim <ezmax.defi@gmail.com>
2023-07-12 15:22:09 -0500

    full db

--- server/app.js
+++ server/app.js
@@ -42,6 +42,7 @@ async function fetchAndUpdate() {
                     },
                 }
             );
+
             console.log(`Updated ${protocol.name} with TVL: ${totalTVL} and MCAP: ${data.mcap}`);
         } catch (error) {
             console.error(`Failed to update ${protocol.name}: ${error.message}`);
@@ -50,6 +51,51 @@ async function fetchAndUpdate() {
 }
 // Fetch & update immediately when the server start
 // fetchAndUpdate(); // <- Uncomment when in Production | Comment in Development
+
+const fetchCGMCaps = async () => {
+    const protocols = await db.protocols.findAll({
+        where: {
+            MCAP: null
+        }
+    });
+
+    let counter = 0
+    for (let protocol of protocols) {
+        counter++;
+        // Check if MCAP is null & fetch data from cg if needed:
+        if(protocol.geckoID){
+            const coingeckoResponse = await axios.get(`https://api.coingecko.com/api/v3/coins/${protocol.geckoID}/market_chart?vs_currency=usd&days=max&interval=daily`);
+            const coingeckoData = coingeckoResponse.data;
+
+            const mcapData = coingeckoData.market_caps.map((price) => ({
+                time: price[0] / 1000,
+                value: price[1],
+            }))
+
+            let mcap = mcapData[mcapData.length - 1].value;
+
+            await db.protocols.update(
+                {
+                    MCAP: mcap
+                },
+                {
+                    where: {
+                        id: protocol.id
+                    }
+                }
+            )
+            console.log(`updated ${protocol.name} with mcap: ${mcap}`);
+        }
+
+        if(counter === 10){
+            return
+        }
+    }
+}
+// fetchCGMCaps()
+const intervalTime = 60 * 1000; // 60 seconds
+
+setInterval(fetchCGMCaps, intervalTime);
 
 // Then fetch and update every 12 hours
 // setInterval(fetchAndUpdate, 24 * 60 * 60 * 1000); // <- Uncomment when in Production | Comment in Development
